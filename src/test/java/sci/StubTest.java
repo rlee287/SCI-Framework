7ad588ec2c03b5d9c8915e2f7b44d50c9c060ca9
rlee287 <rlee287@yahoo.com>
2018-02-06 12:56:15 -0800

    Create stub test to organize test package

---
+++ src/test/java/sci/StubTest.java
@@ -0,0 +1,16 @@
+package sci;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class StubTest {
+
+    @Test
+    public void hi() {
+        System.out.println("hi");
+        Assert.assertNotNull("floithoifh");
+        // Do nothing
+    }
+}
