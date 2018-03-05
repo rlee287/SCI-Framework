3c2b0296eadadab3672bef0166bf13b37302b0f8
rlee287 <rlee287@yahoo.com>
2018-03-04 16:02:27 -0800

    Add tests for specific help items and nonexistent help items

--- src/test/java/sci/modules/HelpTest.java
+++ src/test/java/sci/modules/HelpTest.java
@@ -71,4 +71,65 @@ public class HelpTest {
         methodc.setAccessible(true);
         Object s = method.invoke(null);
     }
+    
+    /**
+     * Test module "help" to ensure that it returns specific help items.
+     * @throws SecurityException 
+     * @throws NoSuchMethodException 
+     * @throws InvocationTargetException 
+     * @throws IllegalArgumentException 
+     * @throws IllegalAccessException 
+     * @throws InstantiationException 
+     */
+    @Test
+    public void helpModuleProcessSpecific() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException {
+        ArrayList<String> listStr = new ArrayList<>();
+        Method method = SCI.class.getDeclaredMethod("init");
+        method.setAccessible(true);
+        method.invoke(null);
+        
+        Reflections reflect = new Reflections("sci.modules");
+        for( Class<?> c : reflect.getSubTypesOf( sci.modules.Module.class ) ) {
+            Module m = null;
+            m = (Module) c.newInstance();
+            for (String inv:m.getInvokers()) {
+                listStr.add(inv);
+            }
+        }
+        
+        Module helpModule = new HelpModule();
+        for (String mod:listStr) {
+            ArrayList<String> modinput = new ArrayList<>();
+            modinput.add(mod);
+            assertTrue(helpModule.process("No NullLens for The Room", modinput).contains(mod));
+        }
+        Method methodc = SCI.class.getDeclaredMethod("shutdown");
+        methodc.setAccessible(true);
+        method.invoke(null);
+    }
+    
+    /**
+     * Test module "help" to ensure that it handles nonexistent help items.
+     * @throws SecurityException 
+     * @throws NoSuchMethodException 
+     * @throws InvocationTargetException 
+     * @throws IllegalArgumentException 
+     * @throws IllegalAccessException 
+     */
+    @Test
+    public void helpModuleProcessNonExistent() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+        ArrayList<String> listStr = new ArrayList<>();
+        Method method = SCI.class.getDeclaredMethod("init");
+        method.setAccessible(true);
+        method.invoke(null);
+        
+        String mod = "I exist unthinking because what does this test matter in the universe's billions of years";
+        ArrayList<String> modinput = new ArrayList<>();
+        modinput.add(mod);
+        Module helpModule = new HelpModule();
+        assertTrue(helpModule.process("No NullLens for The Room", modinput).contains("does not exist"));
+        Method methodc = SCI.class.getDeclaredMethod("shutdown");
+        methodc.setAccessible(true);
+        method.invoke(null);
+    }
 }
